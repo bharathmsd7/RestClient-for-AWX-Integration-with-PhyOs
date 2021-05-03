efc5f92f55f2e79b2681655e8dd08659c89baafa
Bharath <bharathmsd7@outlook.com>
2021-05-03 20:50:41 +0530

    conf updates

--- app/utils/Constants.java
+++ app/utils/Constants.java
@@ -10,4 +10,12 @@ public class Constants {
     public static final String ANSIBLE_INVENTORY_PATH ="/api/v2/inventories/";
     public static final String ANSIBLE_HOSTS_PATH = "/api/v2/hosts/";
 
+    public static final String APP_1_SCM_URL = "https://github.com/bharathmsd7/Ansible-Playbook-Gitlab";
+    public static final String APP_2_SCM_URL = "https://github.com/bharathmsd7/Ansible_Playbook_Mamoto";
+    public static final String APP_3_SCM_URL = "https://github.com/bharathmsd7/Ansible-Playbook-ClamAV";
+
+    public static final String APP_1_PLAYBOOK = "gitlabce-playbook.yml";
+    public static final String APP_2_PLAYBOOK = "mamoto-playbook.yml";
+    public static final String APP_3_PLAYBOOK = "clamav-playbook.yml";
+
 }
