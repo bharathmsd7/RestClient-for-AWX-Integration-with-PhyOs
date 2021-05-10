2f873393c4b8419e81da6893791256888ff93b5d
Bharath <bharathmsd7@outlook.com>
2021-05-10 13:51:39 +0530

    module update

--- app/Module.java
+++ app/Module.java
@@ -1,2 +1,18 @@
-public class Module {
+import com.google.inject.AbstractModule;
+import dao.AnsibleDAO;
+import dao.IAnsibleDAO;
+import org.mongodb.morphia.Datastore;
+
+public class Module extends AbstractModule {
+
+
+    @Override
+    protected void configure() {
+        try {
+            bind(IAnsibleDAO.class).toConstructor(AnsibleDAO.class.getDeclaredConstructor(Datastore.class));
+        }
+        catch (NoSuchMethodException e){
+            e.printStackTrace();
+        }
+    }
 }
